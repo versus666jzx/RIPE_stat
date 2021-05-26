d6ad443a983a219e98f3d75462ad3373e9f3027a
Андрей Гусликов (ГЦМ) <aguslikov@mail>
2021-05-27 00:44:44 +0300

    initial commit

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
 from countries import countries
+from requests import get
+from json import loads, dumps
 
 
-l = 0
+url = 'https://stat.ripe.net/data/country-resource-stats/data.json'
+
 for country in countries:
-    if l == 4:
-        break
-    print(country["code"])
-    l += 1
+    country_code = country["code"]
+    params = {"resource": country_code}
+    params['starttime'] = '2020-12-15'
+    params['endtime'] = '2020-12-30'
+    params['resolution'] = '5m'
+    try:
+        res = get(url, params)
+    except Exception as err:
+        res = None
+        print(f"Ашипка: {err}")
+
+    if res is not None:
+        res = loads(res.text)['data']['stats']
+        print(dumps(res, indent=4))
+    break
