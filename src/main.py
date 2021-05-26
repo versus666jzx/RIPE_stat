6f57a346a98006a0e3df23f2f45939bd368f8163
Андрей Гусликов (ГЦМ) <aguslikov@mail>
2021-05-27 01:39:39 +0300

    initial commit

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 from countries import countries
 from requests import get
 from json import loads, dumps
+from datetime import datetime, timedelta
 
+yesterday = datetime.today().date() - timedelta(days=30)
+today = datetime.today().date()
 
 url = 'https://stat.ripe.net/data/country-resource-stats/data.json'
 
 for country in countries:
     country_code = country["code"]
     params = {"resource": country_code}
-    params['starttime'] = '2020-12-15'
-    params['endtime'] = '2020-12-30'
+    params['starttime'] = yesterday
+    params['endtime'] = today
     params['resolution'] = '5m'
     try:
         res = get(url, params)
